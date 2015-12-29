c9837e9c06d632551858f03c31d4519cc3db0618
PharoDeveloper <alexzund3r@yahoo.com>
2015-12-29 09:42:48 +0600

    Initial

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    
+    var runBtn = document.getElementById('run'),
+        generationsCountField = document.getElementById('generationsCount'),
+        mutationRateField = document.getElementById('mutationRate');
+};
